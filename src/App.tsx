cd0de4118ec3f5a431851e5bd6b214c6da984825
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-06-02 22:32:07 +0800

    feat: Add shims-img

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import styles from '@/_style.module.scss';
 import type { RFC } from '#/fix-types';
 
 const App: RFC = () => {
-    return <div className={styles.App}>hello world</div>;
+    return <div>hello world</div>;
 };
 
 export default App;
---
+++ types/shims-img.d.ts
@@ -0,0 +1,19 @@
+declare module '*.jpeg' {
+    const img: string;
+    export default classes;
+}
+
+declare module '*.jpg' {
+    const img: string;
+    export default classes;
+}
+
+declare module '*.png' {
+    const img: string;
+    export default classes;
+}
+
+declare module '*.gif' {
+    const img: string;
+    export default classes;
+}
